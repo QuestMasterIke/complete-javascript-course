e5b039eb20f1fe6b6ca3ca2e9d9eae51f80bfed4
Nicolai Laptop <nicolai.scheerer@gmail.com>
2020-06-24 18:53:51 +0200

    changes

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,48 +1,15 @@
-function Question (){
-    question: "";
-    answerA: "";
-    answerB: "";
-    answerC:"";
-    correctAnswer: 0;
+function Question (question,answers,correct){
+  this.question=question;
+  this.answers=answers;
+  this.correct=correct;
  
-    function displayAnswer(){
-        return answerA, answerB, answerC;
-    };
-}
-
-A = new Question ();
-A.question ="Hello World";
-A.answerA="Yes!";
-A.answerB="No!";
-A.answerC="Maybe";
-A.correctAnswer=1;
-
-B = new Question();
-B.question="Nicolai?";
-B.answerA="Yes!";
-B.answerB="No!";
-B.answerC="Maybe";
-B.correctAnswer=1;
-
-var questions=[A,B];
-
-function stringConsturct(Question){
-    question=Question.question +"\n 1. "+Question.answerA +"\n 2. "+ Question.answerB + "\n 3. " + Question.answerC;
-    return prompt(question);
 
 }
 
+var q1 =new Question("Hello...?",["No","World","Nico"],1);
+
+var questions=[q1];
 
 
-var run =1;
-while(run){
-    if(stringConsturct(questions[Math.floor(Math.random()*questions.length)])==="exit"){
-        run = 0;
 
-    }
-       
 
-}
-
-console.log("Made a change");
-Another change
