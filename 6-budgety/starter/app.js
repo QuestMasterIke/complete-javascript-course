72e47881db0ec7fe8ffc33ab0fc2914eee892f26
Scheerer Nicolai <nicolai.scheerer@gmail.com>
2020-06-26 11:55:24 +0200

    Budgety Session 2

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -2,7 +2,29 @@
 
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.desciption = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.desciption = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
 
@@ -12,7 +34,31 @@ var budgetController = (function () {
 
 var uiController = (function () {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn:'.add__btn'
+    }
 
+
+    return {
+        getInput: function () {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be exp or inc
+                desciption: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDom: function () {
+            return DOMstrings;
+            
+
+        }
+
+    }
 
 })();
 
@@ -21,26 +67,42 @@ var uiController = (function () {
 // GLOBAL APP CONTROLLER
 
 
-var controller = (function (budgetCtrl,uiCtrl) {
+var controller = (function (budgetCtrl, uiCtrl) {
+
+    var setupEventListeners = function () {
+        var DOM = uiController.getDom();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    }
 
 
     var ctrlAddItem = function()
     {
                 // 1. get input of field
+        var input = uiController.getInput();
+        console.log(input);
         // 2. Add item to budget controller
         // 3. add item to ui
         // 4. calc budget
         // 5. Display budget on UI
-        console.log("Hello");
+    
     };
 
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem );
-
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function () {
+            console.log("Hello - Budgetty started!");
+            setupEventListeners();
         }
-    });
+    };
+  
+})(budgetController, uiController);
 
-})(budgetController,uiController);
+
+controller.init();
